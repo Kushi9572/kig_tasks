e1c7a69aba7838ae3de062e1a21455c3169e9ae4
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-02 10:34:55 +0200

    Final layout

--- layout/Headerr.php
+++ layout/Headerr.php
@@ -6,7 +6,7 @@
     <title>echo $title</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <style>
-            body {
+body {
     background-color:#EF92D5;
     font-family:'Courier New', Courier, monospace;
     color:#680a11;
@@ -72,7 +72,7 @@ footer{
 
 p{
 
-    height: 50px;
+    height: 0px;
     padding-top: 20px;
     padding-bottom:20px ;
     padding-left: 20px;
--- layout/index.php
+++ layout/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php include "Headerr.php" ?>
-    <h3><h2><b>We are group of teachers  who really  love children's and enjoy every moment of teaching and playing with 
-        our students.</b></h3>
+    <h2><b>We are group of teachers  who really  love children's and enjoy every moment of teaching and playing with 
+        our students.</b></h2>
 
 <?php include "Footer1.php" ?>
 
