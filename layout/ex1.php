44f398c822e016a35cb90c4b196c18f2c0467b5e
Kushani Gnanthilaka <116035588+Kushi9572@users.noreply.github.com>
2023-02-02 09:01:06 +0200

    Add tables and Variyables

--- layout/ex1.php
+++ layout/ex1.php
@@ -12,6 +12,37 @@ echo "Hello World ! My name is \"Kushani\"";
     <h3>3.3 Write the PHP code in to display the current date. </h3>
 <?php echo date("d.m.Y")?>
 
+    <h3>3.4 $title1 = variable in PHP. </h3>
+    <?php
+    $title1= "PHP is interesting";
+    echo "<h1>" . $title1 . "</h1>";
+?>
+
+<h3>3.5 Table and variables </h3>
+<?php
+$g1 = 5;
+$g2 = 5;
+$g3 = 5;
+
+echo "
+<table>
+    <tr>
+    <th>S.N </th><th>Name </th><th>Grade </th>
+    </tr>
+    <tr>
+    <td>1</td><td>Pekka </td><td>$g1 </td>
+    </tr>
+    <tr>
+    <td>2</td><td>Johanna</td><td>$g2 </td>
+    </tr>
+    <tr>
+    <td>3</td><td>John </td><td>$g3 </td>
+    </tr>
+
+</table>
+";
+?>
+
 <?php include "footer.php" ?>
 
 
